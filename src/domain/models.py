cadf741c6b5a625c52bbfd2ae0ca0bf1fab93400
Pedrinho <luispedroca@gmail.com>
2026-08-04 14:24:09 -0300

    ajuste o compose, add alembic pra versionamento e outras alterações

--- src/domain/models.py
+++ src/domain/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, func
-from infra.database import Base
+from ..infra.database import Base
 from datetime import datetime
 from sqlalchemy import Sequence, func
 from sqlalchemy.orm import Mapped, mapped_column
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 class ShortUrl(Base):
     __tablename__ = 'short_urls'
 
-    id = Mapped[int] = mapped_column(
+    id: Mapped[int] = mapped_column(
         Integer, 
         Sequence("short_url_id_seq", start=1000), 
         primary_key=True
