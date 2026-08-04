7320bb551878bef828a1fa2f135bbb996becafa9
Pedrinho <luispedroca@gmail.com>
2026-08-04 12:51:45 -0300

    incluindo a logica de encurtar a url

--- src/application/shortener_app.py
+++ src/application/shortener_app.py
@@ -1,8 +1,27 @@
 from urllib.parse import urlparse
+from snowflake_id_toolkit import TwitterSnowflakeIDGenerator
+import base62
+
+class IdGenerator:
+    def __init__(self):
+        self.client_id = TwitterSnowflakeIDGenerator(node_id=1)
+        self.id = self.id_generator()
+
+    def id_generator(self):
+        return self.client_id.generate_next_id()
+    
+    def encode_base62(self):
+        return base62.encode(self.id)
+
+    
 
 def shortener_url(url:str) -> str:
+    print(url)
     if is_url(url):
-        print("Encurtando URL")
+        generator = IdGenerator()
+        base62_url = generator.encode_base62()
+        
+        return f'localhost:8000/{base62_url}'
     else:
         print("Nao é uma URL")
 
--- src/interfaces/api/url_shortener.py
+++ src/interfaces/api/url_shortener.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, Depends, Request, APIRouter
 from domain.schemas import UrlShortener
 
+from application.shortener_app import shortener_url
+
 
 router = APIRouter()
 
 
 @router.post("/shortener")
-def create_url_shortener(url:UrlShortener):
-    return {'url_shortened': 'https://shorturl/b89y',
+def create_url_shortener(body: UrlShortener):
+    return {'url_shortened': shortener_url(url=body.url),
             'message':'url shortener success'
            }
